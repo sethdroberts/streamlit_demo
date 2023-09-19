cf831f8f9c9109d82dbebdc56153d7d279a50ac4
sethdroberts <sethknock@gmail.com>
2023-09-19 00:49:13 +0000

    Add map of filtered data

--- uber_pickups.py
+++ uber_pickups.py
@@ -29,3 +29,8 @@ st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
+
+hour_to_filter = 17
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
