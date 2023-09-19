b6d5bff91ae457906e9d51b580b01b755ea57bcd
sethdroberts <sethknock@gmail.com>
2023-09-19 00:37:46 +0000

    Fix formula error

--- uber_pickups.py
+++ uber_pickups.py
@@ -12,8 +12,8 @@ def load_data(nrows):
   data = pd.read_csv(DATA_URL, nrows=nrows)
   lowercase = lambda x: str(x).lower()
   data.rename(lowercase, axis='columns',inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+  data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+  return data
     
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
